a8fa5426b2a542cbd8a81b7a43c3a896d0bbff30
div2062001 <purohitdivyanshi67@gmail.com>
2024-03-28 16:14:52 +0530

    1

--- src/app/components/right-container/right-container.component.ts
+++ src/app/components/right-container/right-container.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class RightContainerComponent {
 
+  today:boolean=false;
+  week:boolean = true;
+
+  celsius:boolean = true;
+  fahrenheit:boolean=false;
+
+
+// functions to control tab values
+onTodayClick(){
+  this.today = true;
+  this.week = false;
+}
+
+onWeekClick(){
+  this.week = true;
+  this.today = false;
+}
+
+//functions to control metric values
+
+onCelsiusClick(){
+  this.celsius = true;
+  this.fahrenheit = false;
+}
+onFahrenheitClick(){
+  this.fahrenheit = true;
+  this.celsius = false;
+}
+
+
 }
